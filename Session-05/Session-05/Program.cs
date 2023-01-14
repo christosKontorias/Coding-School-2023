51cf03bb93137e1a46e3d86d2df139277fbdadc3
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-01-14 12:38:30 +0200

    Question 1

--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Session_05;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProgramOne programOne = new ProgramOne();
+
+
+
+
+        Console.WriteLine("Session 5 EXERCISE\r\n");
+        Console.WriteLine("Question 1:");
+        Console.WriteLine(programOne.ReverseName());
+        Console.WriteLine();
+        Console.WriteLine("Question 2:");
+
+        Console.ReadLine();
+
+    }
+}
---
+++ Session-05/Session-05/ProgramOne.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    internal class ProgramOne
+    {
+        public string ReverseName()
+        {
+            String name, reverseName;
+            name = "Christos";
+            reverseName = "";
+
+            Console.WriteLine("Name is {0}", name, ".");
+
+            // Find Name Length
+            int length;
+            length = name.Length - 1;
+            while (length >= 0)
+            {
+                reverseName = reverseName + name[length];
+                length--;
+            }
+
+            return "The Reversed name is: " + reverseName + ".";
+        }
+
+    }
+}
