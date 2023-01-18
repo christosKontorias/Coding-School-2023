831479093b13b6d0403a79ff49b230e1b51fcc16
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-01-18 21:49:31 +0200

    Remove Methods from StringParser

--- Session-07/Session-07/StringParser.cs
+++ Session-07/Session-07/StringParser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,7 @@ namespace Session_07
         {
             public string MessageValue { get; set; }
 
-            public virtual string Manipulate(string input)
+            public virtual string Manipulate()
             {
                 return string.Empty;
             }
@@ -24,7 +25,7 @@ namespace Session_07
             {
                 // “Convert” you must check if the Input is a decimal number and convert it to binary.
 
-                return string.Empty;
+                return String.Empty;
             }
         }
 
@@ -34,23 +35,8 @@ namespace Session_07
             {
                 //“Uppercase” you must check if the Input is a string and has more than
                 //one words(separated by a space), then find the longest word in the
-                //Input string and convert it to                 //if (input.Contains(" "))
-                //{
-                //    string[] words = input.Split(' ');
-                //    string longestWord = "";
-                //    foreach (string word in words)
-                //    {
-                //        if (word.Length > longestWord.Length)
-                //        {
-                //            longestWord = word;
-                //        }
-                //    }
-                //    return longestWord.ToUpper();
-                //}
-                //else
-                //{
-                //    return "Input must contain multiple words.";
-                //}
+                //Input string and convert it to uppercase.   
+
 
                 return string.Empty;
             }
@@ -61,23 +47,6 @@ namespace Session_07
             public override string Manipulate()
             {
                 //“Reverse” you must check if the Input is a string and reverse it.
-
-                //try
-                //{
-                //    if (input.Length > 0)
-                //    {
-                //        return input[input.Length - 1] + Reverse(input.Substring(0, input.Length - 1));
-                //    }
-                //    else
-                //    {
-                //        return input;
-                //    }
-                //}
-                //catch (Exception ex)
-                //{
-                //    return null;
-
-                //}
                 return string.Empty;
             }
         }
