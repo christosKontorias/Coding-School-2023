c215a328c1e945e0da12e708b6d58912b779ac16
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-01-19 16:59:43 +0200

    Check If we divide with 0

--- Sessions-09/CalculatorLibrary/Division.cs
+++ Sessions-09/CalculatorLibrary/Division.cs
@@ -7,14 +7,16 @@
             decimal ret = 0;
 
             if (x != null && y != null)
-            { 
-                if ( x != 0 && y != 0)
+            {
+                if (x != 0 && y != 0)
                 {
-                    ret = x.Value / y.Value;
+                    if (y != 0) { 
+                        ret = x.Value / y.Value;
+                    }
+ 
                 }
             }
             return ret;
         }
-
     }
 }
