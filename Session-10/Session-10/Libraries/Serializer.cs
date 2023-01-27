2a7cc808c0df3c9135f856a1506bdcd911fa8b66
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-01-27 19:40:39 +0200

    Create New Folder, +Serializer

---
+++ Session-10/Session-10/Libraries/Serializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Session_10.Libraries {
+    public class Serializer {
+
+        public void SerializeToFile(object obj, string filename) {
+            string jsonString = JsonSerializer.Serialize(obj);
+            File.WriteAllText(filename, jsonString);
+        }
+        public T DeserializeFromFile<T>(string fileName) {
+
+            string jsonString = File.ReadAllText(fileName);
+            T? obj = JsonSerializer.Deserialize<T>(jsonString);
+            return obj;
+        }
+    }
+}
