89fc67d3c779faf9b202eb91266b7a9bd5299cf5
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-01-31 13:44:38 +0200

    +transactionLineConfiguration

--- Session-16/CarServiceCenterLib.Orm/Configurations/TransactionLineConfiguration.cs
+++ Session-16/CarServiceCenterLib.Orm/Configurations/TransactionLineConfiguration.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CarServiceCenterLib.Models;
 
 namespace CarServiceCenterLib.Orm.Configurations {
-    internal class TransactionLineConfiguration {
+    public class TransactionLineConfiguration : IEntityTypeConfiguration<TransactionLine> {
+        public void Configure(EntityTypeBuilder<TransactionLine> builder) {
+            builder.ToTable("TransactionLines");
+            builder.HasKey(transactionline => transactionline.ID);
+
+
+            builder.HasOne(transactionline => transactionline.Transaction)
+                 .WithMany(transaction => transaction.TransactionLines)
+                 .HasForeignKey(transactionline => transactionline.TransactionID);
+        }
     }
 }
