b14cc0d3bd7df80451f35d314c69628071d1d6d7
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-02-07 15:15:34 +0200

    Complete

--- Session-22/script.js
+++ Session-22/script.js
@@ -5,9 +5,9 @@ function reverseString(str) {
 }
 
 document.getElementById("reverseButton").addEventListener("click", function () {
-    var input = document.getElementById("input").value;
-    var reversedString = reverseString(input);
-    document.getElementById("output").innerHTML = "Reversed: " + reversedString;
+    var inputQuestion1 = document.getElementById("inputQuestion1").value;
+    var reversedString = reverseString(inputQuestion1);
+    document.getElementById("outputQuestion1").innerHTML = "Reversed: " + reversedString;
 });
 
 /* Question 2*/
@@ -18,45 +18,93 @@ function isPalindrome(str) {
 }
 
 document.getElementById("checkButton").addEventListener("click", function () {
-    var inputString = document.getElementById("inputPalindrome").value;
+    var inputString = document.getElementById("inputQuestion2").value;
     if (isPalindrome(inputString)) {
-        document.getElementById("outputPalindrome").innerHTML = "The string is a palindrome.";
+        document.getElementById("outputQuestion2").innerHTML = "The string is a palindrome.";
     } else {
-        document.getElementById("outputPalindrome").innerHTML = "The string is not a palindrome.";
+        document.getElementById("outputQuestion2").innerHTML = "The string is not a palindrome.";
     }
 });
 
 /* Question 3*/
 
+let customers = [];
+
+function saveCustomer() {
+    let name = document.querySelector('#name').value;
+    let surname = document.querySelector('#surname').value;
+    let age = document.querySelector('#age').value;
+    let gender = document.querySelector('#gender').value;
+
+    customers.push({ name, surname, age, gender });
+    renderCustomers();
+}
+
+function renderCustomers() {
+    let customerTableBody = document.querySelector('#customerTableBody');
+    customerTableBody.innerHTML = '';
+
+    customers.forEach(customer => {
+        let tr = document.createElement('tr');
+        tr.innerHTML = `
+      <td>${customer.name}</td>
+      <td>${customer.surname}</td>
+      <td>${customer.age}</td>
+      <td>${customer.gender}</td>
+    `;
+        tr.addEventListener('click', () => {
+            renderCustomerDetails(customer);
+        });
+        customerTableBody.appendChild(tr);
+    });
+}
+
+function renderCustomerDetails(customer) {
+    let displayName = document.querySelector('#displayName');
+    let displaySurname = document.querySelector('#displaySurname');
+    let displayAge = document.querySelector('#displayAge');
+    let displayGender = document.querySelector('#displayGender');
+
+    displayName.value = customer.name;
+    displaySurname.value = customer.surname;
+    displayAge.value = customer.age;
+    displayGender.value = customer.gender;
+}
+
 /* Question 4*/
 
 function multiply(a, b) {
     return a * b;
-  }
+}
 
-  const multiplyBtn = document.getElementById("multiply");
-  multiplyBtn.addEventListener("click", function() {
+const multiplyBtn = document.getElementById("multiply");
+multiplyBtn.addEventListener("click", function () {
     const numberA = parseInt(document.getElementById("numberA").value);
     const numberB = parseInt(document.getElementById("numberB").value);
-    const result = multiply(numberA, numberB);
-    document.getElementById("result").innerHTML = "Result: " + result;
-  });
+    const outputQuestion4 = multiply(numberA, numberB);
+    if (isNaN(numberA) || isNaN(numberB)) {
+        document.getElementById("outputQuestion4").innerHTML = "Both parameters must be numbers.";
+    } else {
+        const outputQuestion4 = multiply(numberA, numberB);
+        document.getElementById("outputQuestion4").innerHTML = "Result: " + outputQuestion4;
+    }
+});
 
 /* Question 5*/
 
-      function modifyString(str) {
-        const match = str.match(/\d+$/);
-        if (match) {
-          const number = parseInt(match[0]) + 1;
-          return str.substring(0, str.lastIndexOf(match[0])) + number;
-        } else {
-          return str + "1";
-        }
-      }
+function modifyString(str) {
+    const match = str.match(/\d+$/);
+    if (match) {
+        const number = parseInt(match[0]) + 1;
+        return str.substring(0, str.lastIndexOf(match[0])) + number;
+    } else {
+        return str + "1";
+    }
+}
 
-      const modifyBtn = document.getElementById("modify");
-      modifyBtn.addEventListener("click", function() {
-        const inputQuestion5 = document.getElementById("inputQuestion5").value;
-        const outputQuestion5 = modifyString(inputQuestion5);
-        document.getElementById("outputQuestion5").innerHTML = "Modified string: " + outputQuestion5;
-      });
+const modifyBtn = document.getElementById("modify");
+modifyBtn.addEventListener("click", function () {
+    const inputQuestion5 = document.getElementById("inputQuestion5").value;
+    const outputQuestion5 = modifyString(inputQuestion5);
+    document.getElementById("outputQuestion5").innerHTML = "Modified string: " + outputQuestion5;
+});
