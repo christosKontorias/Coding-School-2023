f6ef1faa2984e2dd551ee95777cb0f671499b83f
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-02-08 16:48:51 +0200

    +CarServiceCenter.Web.Mvc

---
+++ Session-23/CarServiceCenter.Web.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace CarServiceCenter.Web.Mvc.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
