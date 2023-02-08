9cb5ef9d5cae44d6c20fad2f1d790e8c9267a540
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-02-08 16:31:56 +0200

    Remove Session-23

--- Session-23/CarServiceCenter.Web.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace CarServiceCenter.Web.Mvc.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
