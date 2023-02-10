5acc94079c05b6f0ca452e3e8961e57cbaa8f461
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-02-10 12:53:16 +0200

    Fix CustomerRepo, CarRepo

--- Session-23/CarServiceCenter.EF/Repositories/CustomerRepo.cs
+++ Session-23/CarServiceCenter.EF/Repositories/CustomerRepo.cs
@@ -1,5 +1,6 @@
 ﻿using CarServiceCenter.EF.Context;
 using CarServiceCenter.Model;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,20 @@ using System.Threading.Tasks;
 namespace CarServiceCenter.EF.Repositories {
     public class CustomerRepo : IEntityRepo<Customer> {
         public void Add(Customer entity) {
-            throw new NotImplementedException();
+            using var context = new CarServiceCenterDbContext();
+
+            if (entity.Id != 0)
+                throw new ArgumentException("Given entity should not have Id set", nameof(entity));
+
+            context.Customers.Add(entity);
+            context.SaveChanges();
         }
 
         public void Delete(int id) {
             using var context = new CarServiceCenterDbContext();
             var CustomerDb = context.Customers.Where(customer => customer.Id == id).SingleOrDefault();
             if (CustomerDb is null)
-                return;
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
             context.Remove(CustomerDb);
             context.SaveChanges();
         }
@@ -28,18 +35,25 @@ namespace CarServiceCenter.EF.Repositories {
 
         public Customer? GetById(int id) {
             using var context = new CarServiceCenterDbContext();
-            return context.Customers.SingleOrDefault();
+            var CustomerDb = context.Customers.Where(customer => customer.Id == id).SingleOrDefault();
+            if (CustomerDb is null) {
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            } else {
+                return CustomerDb;
+            }
         }
 
         public void Update(int id, Customer entity) {
             using var context = new CarServiceCenterDbContext();
             var CustomerDb = context.Customers.Where(customer => customer.Id == id).SingleOrDefault();
-            if (CustomerDb is null)
-                return;
+            if (CustomerDb is null) 
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            
             CustomerDb.Name = entity.Name;
             CustomerDb.Surname = entity.Surname;
             CustomerDb.Phone = entity.Phone;
             CustomerDb.Tin = entity.Tin;
+
             context.SaveChanges();
         }
     }
