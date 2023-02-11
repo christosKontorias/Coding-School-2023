04a453e70d2b587b51d26a5af7f075188f9186b1
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-02-11 21:57:53 +0200

    Transaction

--- Session-23/CarServiceCenter.Web.Mvc/Controllers/TransactionController.cs
+++ Session-23/CarServiceCenter.Web.Mvc/Controllers/TransactionController.cs
@@ -1,21 +1,35 @@
 ﻿using CarServiceCenter.EF.Repositories;
 using CarServiceCenter.Model;
 using CarServiceCenter.Web.Mvc.Models.Transaction;
+using CarServiceCenter.Web.Mvc.Models.TransactionLine;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Transactions;
 
 namespace CarServiceCenter.Web.Mvc.Controllers {
     public class TransactionController : Controller {
 
         private readonly IEntityRepo<Transaction> _transactionRepo;
+        private readonly IEntityRepo<Customer> _customerRepo;
+        private readonly IEntityRepo<Manager> _managerRepo;
+        private readonly IEntityRepo<Car> _carRepo;
 
-        public TransactionController(IEntityRepo<Transaction> transactionRepo) {
+
+        public TransactionController(IEntityRepo<Transaction> transactionRepo,
+                                     IEntityRepo<Customer> customerRepo,
+                                     IEntityRepo<Manager> managerRepo,
+                                     IEntityRepo<Car> carRepo) {
             _transactionRepo = transactionRepo;
+            _customerRepo = customerRepo;
+            _managerRepo = managerRepo;
+            _carRepo = carRepo;
         }
+
         // GET: TransactionController
         public ActionResult Transaction() {
             var transactions = _transactionRepo.GetAll();
             return View(model: transactions);
+
         }
 
         // GET: TransactionController/Details/5
@@ -25,7 +39,23 @@ namespace CarServiceCenter.Web.Mvc.Controllers {
 
         // GET: TransactionController/Create
         public ActionResult Create() {
-            return View();
+            var newTransaction = new TransactionCreateDto();
+            var customers = _customerRepo.GetAll();
+            foreach (var customer in customers) {
+                newTransaction.Customers.Add(new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem(customer.Surname, customer.Id.ToString()));
+            }
+
+            var managers = _managerRepo.GetAll();
+            foreach (var manager in managers) {
+                newTransaction.Managers.Add(new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem(manager.Surname, manager.Id.ToString()));
+            }
+
+            var cars = _carRepo.GetAll();
+            foreach (var car in cars) {
+                newTransaction.Cars.Add(new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem(car.Model, car.Id.ToString()));
+            }
+
+            return View(model: newTransaction);
         }
 
         // POST: TransactionController/Create
--- Session-23/CarServiceCenter.Web.Mvc/Models/Transaction/TransactionCreateDto.cs
+++ Session-23/CarServiceCenter.Web.Mvc/Models/Transaction/TransactionCreateDto.cs
@@ -4,9 +4,15 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 namespace CarServiceCenter.Web.Mvc.Models.Transaction; 
 public class TransactionCreateDto {
     public DateTime Date { get; set; }
-    //public decimal TotalPrice { get; set; }
-    //public int CustomerId { get; set; }
-    //public int ManagerId { get; set; }
-    //public int CarId { get; set; }
-   // public List<SelectListItem> TransactionLines { get;} = new List<SelectListItem>();
+    public decimal TotalPrice { get; set; }
+    public int CustomerId { get; set; }
+    public List<SelectListItem> Customers { get; } = new List<SelectListItem>();
+
+    public int ManagerId { get; set; }
+    public List<SelectListItem> Managers { get; } = new List<SelectListItem>();
+
+    public int CarId { get; set; }
+    public List<SelectListItem> Cars { get; } = new List<SelectListItem>();
+
+    public List<SelectListItem> TransactionLines { get; } = new List<SelectListItem>();
 }
