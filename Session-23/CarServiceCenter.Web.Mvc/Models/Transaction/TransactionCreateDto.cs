d4f12a5ef7b94a57c7adc8608881a032afca9025
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-02-11 17:21:38 +0200

    Transaction Models CRUD

--- Session-23/CarServiceCenter.Web.Mvc/Models/Transaction/TransactionCreateDto.cs
+++ Session-23/CarServiceCenter.Web.Mvc/Models/Transaction/TransactionCreateDto.cs
@@ -1,5 +1,12 @@
-﻿namespace CarServiceCenter.Web.Mvc.Models.Transaction; 
+﻿using CarServiceCenter.Web.Mvc.Models.TransactionLine;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CarServiceCenter.Web.Mvc.Models.Transaction; 
 public class TransactionCreateDto {
     public DateTime Date { get; set; }
     public decimal TotalPrice { get; set; }
+    public int CustomerId { get; set; }
+    public int ManagerId { get; set; }
+    public int CarId { get; set; }
+    public List<SelectListItem> TransactionLines { get;} = new List<SelectListItem>();
 }
--- Session-23/CarServiceCenter.Web.Mvc/Models/Transaction/TransactionDetailsDto.cs
+++ Session-23/CarServiceCenter.Web.Mvc/Models/Transaction/TransactionDetailsDto.cs
@@ -1,7 +1,13 @@
-﻿namespace CarServiceCenter.Web.Mvc.Models.Transaction {
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CarServiceCenter.Web.Mvc.Models.Transaction {
     public class TransactionDetailsDto {
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public decimal TotalPrice { get; set; }
+        public int CustomerId { get; set; }
+        public int ManagerId { get; set; }
+        public int CarId { get; set; }
+        public List<SelectListItem> TransactionLines { get; set; } = new List<SelectListItem>();
     }
 }
