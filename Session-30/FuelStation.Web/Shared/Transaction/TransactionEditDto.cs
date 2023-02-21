ad5a8d775c1598e4926aa5bc4efd36e07b1deb68
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-02-21 22:01:36 +0200

    Transaction,TransactionLine DTO

---
+++ Session-30/FuelStation.Web/Shared/Transaction/TransactionEditDto.cs
@@ -0,0 +1,20 @@
+﻿using FuelStation.Model.Enums;
+using FuelStation.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.Web.Shared.Transaction {
+	public class TransactionEditDto {
+		public int Id { get; set; }
+		public DateTime Date { get; set; }
+		public PaymentMethod PaymentMethod { get; set; }
+		public decimal TotalValue { get; set; }
+		public int EmployeeId { get; set; }
+		public int CustomerId { get; set; }
+
+		public List<TransactionLine> TransactionLines { get; set; } = new();
+	}
+}
---
+++ Session-30/FuelStation.Web/Shared/TransactionLine/TransactionLineListDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.Web.Shared.TransactionLine {
+	public class TransactionLineListDto {
+		public int Id { get; set; }
+		public int Quantity { get; set; }
+		public decimal ItemPrice { get; set; }
+		public decimal NetValue { get; set; }
+		public decimal DiscountPercent { get; set; }
+		public decimal DiscountValue { get; set; }
+		public decimal TotalValue { get; set; }
+		public int TransactionId { get; set; }
+		public int ItemId { get; set; }
+	}
+}
