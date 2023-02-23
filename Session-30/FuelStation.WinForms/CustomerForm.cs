ef1df9bb88cfa667754b420891eaa76a8bbc73fe
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-02-23 17:28:23 +0200

    Winform Customer

--- Session-30/FuelStation.WinForms/CustomerForm.cs
+++ Session-30/FuelStation.WinForms/CustomerForm.cs
@@ -1,7 +1,39 @@
+using FuelStation.Web.Shared.Customer;
+using Newtonsoft.Json;
+
 namespace FuelStation.WinForms {
 	public partial class CustomerForm : Form {
+
+		private readonly HttpClient _client;
+
 		public CustomerForm() {
 			InitializeComponent();
+
+			_client = new HttpClient();
+			_client.BaseAddress = new Uri("https://localhost:7136/");
+		}
+
+		private void CustomerForm_Load(object sender, EventArgs e) {
+			dataGridView1.AutoGenerateColumns = false;
+			SetControlProperties();
+		}
+
+		private async Task SetControlProperties() {
+
+			var customers = await GetCustomers();
+			if (customers != null) {
+				bsCustomers.DataSource = customers;
+				dataGridView1.DataSource = bsCustomers;
+			}
+		}
+
+		private async Task<List<CustomerListDto>> GetCustomers() {
+			var response = await _client.GetAsync("customer");
+			if (response.IsSuccessStatusCode) {
+				var content = await response.Content.ReadAsStringAsync();
+				return JsonConvert.DeserializeObject<List<CustomerListDto>>(content);
+			}
+			return null;
 		}
 	}
 }
